5e8bd9b4f42e662ba3fd3ee2018b78aaa76a9fa5
Grant <grantmuse@gmail.com>
2021-08-24 15:14:55 -0400

    Added job/jobs/careers/career to the sql query

--- src/main/scala/cc/idx/CCIdxMain.scala
+++ src/main/scala/cc/idx/CCIdxMain.scala
@@ -9,7 +9,8 @@ object CCIdxMain {
     def main(args: Array[String]): Unit = {
 		val tablePath = "s3a://commoncrawl/cc-index/table/cc-main/warc/"
 		val viewName = "ccindex"
-		val sqlQuery = "Select * From " + viewName + " Where crawl=\'CC-MAIN-2021-10\' And subset=\'warc\'"
+			val sqlQuery = "Select * From " + viewName + " Where crawl=\'CC-MAIN-2021-10\' And subset=\'warc\' AND url RLIKE \'.*(/job/|/jobs/|/careers/|/career/).*\'"
+
 
 			val conf = new SparkConf().setAppName(this.getClass.getCanonicalName())
 			val spark = SparkSession.builder.master("local[*]").config(conf).getOrCreate
