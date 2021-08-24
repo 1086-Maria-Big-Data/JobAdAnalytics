f118cf6e15257c60856f37d802446e15355424c0
Grant <grantmuse@gmail.com>
2021-08-24 14:54:54 -0400

    Index query functional

--- src/main/scala/cc/idx/CCIdxMain.scala
+++ src/main/scala/cc/idx/CCIdxMain.scala
@@ -9,15 +9,15 @@ object CCIdxMain {
     def main(args: Array[String]): Unit = {
 		val tablePath = "s3a://commoncrawl/cc-index/table/cc-main/warc/"
 		val viewName = "ccindex"
-		val sqlQuery = "Select * From " + viewName + " Where crawl=CC-MAIN-2021-10 And subset=warc"
+		val sqlQuery = "Select * From " + viewName + " Where crawl=\'CC-MAIN-2021-10\' And subset=\'warc\'"
 
-		val conf = new SparkConf().setAppName(this.getClass.getCanonicalName())
-        val spark = SparkSession.builder.master("local").config(conf).getOrCreate
-		val sc = spark.sparkContext
-		
+			val conf = new SparkConf().setAppName(this.getClass.getCanonicalName())
+			val spark = SparkSession.builder.master("local[*]").config(conf).getOrCreate
+			val sc = spark.sparkContext
+			spark.conf.set("spark.sql.debug.maxToStringFields", 1000)
 		val df = spark.read.load(tablePath)
-		df.createOrReplaceGlobalTempView(viewName)
-		spark.sql(sqlQuery)
+		df.createOrReplaceTempView(viewName)
+		spark.sql(sqlQuery).show
 
 		spark.stop
 
--- src/main/scala/mainObject.scala
+++ src/main/scala/mainObject.scala
@@ -8,6 +8,9 @@ object mainObject {
     /**
       * Just a reminder, if you'd like to test it, you'll need to set the
       * AWS_ACCESS_KEY_ID and AWS_SECRET_ACESS_KEY environment variables
+     *
+     * To set environment vairable in IntelliJ click on the dropdown with mainObject and edit configuration
+     * then set AWS_ACCESS_KEY_ID=******;AWS_SECRET_ACCESS_KEY=*******
       */
     CCIdxMain.main(args)
   }
