6adbcbeb09ec2eb2716de4897f7d65875344fbb8
Vincent Chooi <vinceecws@gmail.com>
2021-08-25 19:49:54 -0400

    Updated config instructions in mainObject.scala

--- src/main/scala/mainObject.scala
+++ src/main/scala/mainObject.scala
@@ -7,13 +7,9 @@ object mainObject {
     // Sample run -- still WIP
     /**
      * Just a reminder, if you'd like to test it, you'll need to set the
-     * AWS_ACCESS_KEY_ID and AWS_SECRET_ACESS_KEY environment variables
-     *
-     * How to get access keys:
-     * AWS -> your account dropdown -> My security credentials -> Access Keys -> create new access key
-     *
-     * To set environment vairable in IntelliJ click on the dropdown with mainObject and edit configuration
-     * then set environment variable to AWS_ACCESS_KEY_ID=******;AWS_SECRET_ACCESS_KEY=*******
+     * AWS_ACCESS_KEY_ID and AWS_SECRET_ACESS_KEY config variables
+     * 
+     * Checkout README.md for instructions.
      */
 
     CCIdxMain.main(args)
