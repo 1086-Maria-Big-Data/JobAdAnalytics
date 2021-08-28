49f0f9d3c775a8bcb35002eae81333d6dd593a39
Vincent Chooi <vinceecws@gmail.com>
2021-08-28 03:01:00 -0400

    Removed unnecessary imports

--- src/main/scala/cc/idx/CCIdxMain.scala
+++ src/main/scala/cc/idx/CCIdxMain.scala
@@ -4,7 +4,6 @@ import cc.warc._
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Dataset, DataFrame}
-import org.apache.spark.sql.types.{StructType, StructField, IntegerType, TimestampType, StringType, ShortType}
 import org.apache.spark.sql.functions._
 
 import org.archive.archivespark._
@@ -12,11 +11,6 @@ import org.archive.archivespark.specific.warc._
 import org.archive.archivespark.specific.warc.functions._
 
 import spark.session.AppSparkSession
-import org.archive.archivespark.model.dataloads.ByteLoad
-
-import org.archive.archivespark.sparkling.cdx.CdxRecord
-
-import java.io.{InputStream, ByteArrayInputStream}
 
 /**
  * CCIdxMain is used for querying the index table from common crawl's S3 bucket
@@ -50,13 +44,17 @@ object CCIdxMain {
         /**
           * Testing capacity to manually make a CdxRecord from ccindex table to select specific warc records
           */
-        
-        val forCdxRec = spark.sql("SELECT url_surtkey, fetch_time, url, content_mime_type, fetch_status, content_digest, fetch_redirect, warc_segment, warc_record_length, warc_record_offset, warc_filename"
-            + " from " + viewName + " Where crawl=\'CC-MAIN-2021-10\' And subset=\'warc\' AND url RLIKE \'.*(/job/|/jobs/|/careers/|/career/).*\'")
+
+        val forCdxRec = df
+            .select("url_surtkey", "fetch_time", "url", "content_mime_type", "fetch_status", "content_digest", "fetch_redirect", "warc_segment", "warc_record_length", "warc_record_offset", "warc_filename")
+            .where(col("crawl") === "CC-MAIN-2021-10" && col("subset") === "warc" && col("url_path").rlike(".*(/job/|/jobs/|/careers/|/career/).*"))
 
         val warc_rdd = WarcUtil.loadFiltered(forCdxRec)
 
-        warc_rdd.map(warc => SuperWarc(warc)).take(5).foreach(warc => println(warc.payload()))
+        warc_rdd.map(warc => SuperWarc(warc)).take(5).foreach(warc => println(warc.payload(true)))
+
+        // val warc_rdd = WarcUtil.load("s3a://commoncrawl/crawl-data/CC-MAIN-2021-31/segments/1627046149929.88/warc/CC-MAIN-20210723143921-20210723173921-00000.warc.gz")
+        // warc_rdd.take(5).foreach(warc => println(warc.toJsonString))
 
         spark.stop
 
