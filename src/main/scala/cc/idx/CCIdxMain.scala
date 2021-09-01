9d74ad7cf2c0991f247cb5dfbea0f83b8c78d759
brian <brja3022@colorado.edu>
2021-09-01 09:42:15 -0600

    Preliminary tech job trends query

--- src/main/scala/cc/idx/CCIdxMain.scala
+++ src/main/scala/cc/idx/CCIdxMain.scala
@@ -10,6 +10,9 @@ import org.archive.archivespark._
 import org.archive.archivespark.specific.warc._
 import org.archive.archivespark.specific.warc.functions._
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 import spark.session.AppSparkSession
 
 /**
@@ -39,22 +42,19 @@ object CCIdxMain {
         /**
          * loading the index to dataframe(df)
          */
-        // val df = spark.read.schema(IndexUtil.schema).parquet(tablePath)
+        val df = spark.read.schema(IndexUtil.schema).parquet(tablePath)
         
         /**
           * Testing capacity to manually make a CdxRecord from ccindex table to select specific warc records
           */
 
-        // val forCdxRec = df
-        //     .select("url_surtkey", "fetch_time", "url", "content_mime_type", "fetch_status", "content_digest", "fetch_redirect", "warc_segment", "warc_record_length", "warc_record_offset", "warc_filename")
-        //     .where(col("crawl") === "CC-MAIN-2021-10" && col("subset") === "warc" && col("url_path").rlike(".*(/job/|/jobs/|/careers/|/career/).*"))
+        val forCdxRec = df
+            .select("url_surtkey", "fetch_time", "url", "content_mime_type", "fetch_status", "content_digest", "fetch_redirect", "warc_segment", "warc_record_length", "warc_record_offset", "warc_filename")
+            .where(col("crawl") === "CC-MAIN-2021-10" && col("subset") === "warc" && col("url_path").rlike(".*(/job/|/jobs/|/careers/|/career/).*"))
 
-        // val warc_rdd = WarcUtil.loadFiltered(forCdxRec)
+        val warc_rdd = WarcUtil.loadFiltered(forCdxRec)
 
-        // warc_rdd.map(warc => SuperWarc(warc)).take(5).foreach(warc => println(warc.payload(true)))
-
-        val warc_rdd = WarcUtil.load("s3a://commoncrawl/crawl-data/CC-MAIN-2021-31/segments/1627046149929.88/warc/CC-MAIN-20210723143921-20210723173921-00000.warc.gz")
-        warc_rdd.take(5).foreach(warc => println(warc.toJsonString))
+        warc_rdd.map(warc => SuperWarc(warc)).take(5).foreach(warc => println(warc.payload(true)))
 
         spark.stop
 
@@ -65,27 +65,49 @@ object CCIdxMain {
 object TestExtract {
 
     def main(args: Array[String]): Unit = {
-        /**
-         * Building the spark session
-         */
-
         val spark = AppSparkSession()
+
+        /**
+          * Example 1: Load one WARC file from CommonCrawl S3, perform word count on 5 records and write as CSV to S3 bucket.
+          */
+        val warc_rdd = WarcUtil.load("s3a://commoncrawl/crawl-data/CC-MAIN-2021-31/segments/1627046149929.88/warc/CC-MAIN-20210723143921-20210723173921-00000.warc.gz")
+
+        val wordCount_df = spark.createDataFrame(
+            warc_rdd
+                .take(5)
+                .map(warc => SuperWarc(warc))
+                .flatMap(warc => warc.payload(true).split(" "))
+                .map(word => (word, 1))
+                .groupBy(_._1)
+                .map(pair => (pair._1, pair._2.map(_._2).reduce(_ + _)))
+                .toSeq
+        )
+
+        IndexUtil.write(wordCount_df, "s3a://maria-1086/Brian_Testing" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")), include_header=true, num_files=1)
+/*
+        /**
+          * Example 2: Query CommonCrawl Index, get WARC record offsets, perform word count on all records and write as CSV to S3 bucket.
+          */
 
         val tablePath = "s3a://commoncrawl/cc-index/table/cc-main/warc"
 
         val df = spark.read.schema(IndexUtil.schema).parquet(tablePath)
 
         val forCdxRec = df
-            .select("url_surtkey","fetch_time","url","content_mime_type","fetch_status","content_digest","fetch_redirect","warc_segment","warc_record_length","warc_record_offset","warc_filename")
-            .where("crawl=\'CC-MAIN-2021-10\' And subset=\'warc\' AND url RLIKE \'.*(/job/|/jobs/|/careers/|/career/).*\'")
+            .select("url_surtkey", "fetch_time", "url", "content_mime_type", "fetch_status", "content_digest", "fetch_redirect", "warc_segment", "warc_record_length", "warc_record_offset", "warc_filename")
+            .where(col("crawl") === "CC-MAIN-2021-10" && col("subset") === "warc" && col("content_languages") === "eng" && col("url_path").rlike(".*(/job/|/jobs/|/careers/|/career/).*"))
 
+        val warc_rdd2 = WarcUtil.loadFiltered(forCdxRec).repartition(640)
 
-        val warc_rdd = WarcUtil.loadFiltered(forCdxRec)
+        val wordCount_df2 = spark.createDataFrame(
+            warc_rdd2
+                .map(warc => SuperWarc(warc))
+                .flatMap(warc => warc.payload(true).split(" "))
+                .map(word => (word, 1))
+                .reduceByKey(_ + _)
+        )
 
-        warc_rdd.take(1).foreach(x => println(x.toJsonString))
-
-        spark.stop
-
-        System.exit(0)
+        IndexUtil.write(wordCount_df2, "s3://maria-1086/Vince-Test-CCIdxMain/outputs/" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")), include_header=true, num_files=64)
+*/
     }
 }
--- src/main/scala/cc/queries/techJobs.scala
+++ src/main/scala/cc/queries/techJobs.scala
@@ -1,5 +1,26 @@
 package cc.queries
 
+import cc.warc.WarcUtil
+import org.archive.archivespark.functions.{Html, HtmlText}
+import spark.session.AppSparkSession
+
 object techJobs extends Queries {
+  def main(args: Array[String]): Unit = {
+    val spark = AppSparkSession()
+
+    val rdd=WarcUtil.load(path="s3a://commoncrawl/crawl-data/CC-MAIN-2014-23/segments/1405997885796.93/warc/CC-MAIN-20140722025805-00016-ip-10-33-131-23.ec2.internal.warc.gz")
+    println(rdd.take(1))
+
+    val months = Array[String]("January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+    var monthlyPages = Array[Int](0,0,0,0,0,0,0,0,0,0,0,0)
+
+    for (i <- 0 to 11) {
+
+      val xxt = rdd.enrich(HtmlText.ofEach(Html.all("body"))).toJsonStrings.take(1000).map { r => r.split(" ").mkString("Array(", ", ", ")").contains(months(i)) }
+
+      println(xxt.count(_ == true))
+    }
+
+  }
     
 }
