20d2734efd61fe9cb1abe1d93fd7502acf2672f0
mtorres1127 <87192726+mtorres1127@users.noreply.github.com>
2021-09-02 15:28:51 -0600

    mark commit2

--- src/main/scala/cc/queries/jobSpikes.scala
+++ src/main/scala/cc/queries/jobSpikes.scala
@@ -9,13 +9,13 @@ object jobSpikes extends Queries {
         jobSpikesByJob(path)
         
         def jobSpikesByJob(path:String): Unit = {
-        val spark = AppSparkSession()
-      
-        val df = spark.read.format("csv").option("header", "true").load(path)
-        df.createOrReplaceTempView("dat")
-        spark.sql("SELECT (select count(url) from dat where url like '%java%') as java, (SELECT count(url) from dat where url like '%python%') as python, (SELECT count(url) from dat where url like '%scala%') as scala, (SELECT count(url) from dat where url like '%matlab%') as matlab, (SELECT count(url) from dat where url like '%SQL%') as sql from dat limit 1").show()
+            val spark = AppSparkSession()
+            val df = spark.read.format("csv").option("header", "true").load(path)
+            val df2 = 
+            df.createOrReplaceTempView("dat")
+            spark.sql("SELECT (select count(url) from dat where LOWER(url) like '%java%') as java, (SELECT count(url) from dat where LOWER(url) like '%python%') as python, (SELECT count(url) from dat where LOWER(url) like '%scala%') as scala, (SELECT count(url) from dat where LOWER(url) like '%matlab%') as matlab, (SELECT count(url) from dat where LOWER(url) like '%sql%') as sql from dat limit 1").show()
         
-      // val df_covid_19_data = spark.read.format("csv").option("header", "true").load("input/covid_19_data.csv")
+        // val df_covid_19_data = spark.read.format("csv").option("header", "true").load("input/covid_19_data.csv")
         //val rddFromFile = spark.sparkContext.textFile("s3a://maria-1086/Testing/Mark-Testing/part-00001-bb6fa4ba-4e14-49d3-985c-e570505dc35d-c000.csv",2)
         //val rdd = rddFromFile.map(f =>{f.split(",")})
         //rddFromFile.foreach(println)
