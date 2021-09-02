b6369acc6d008fc2a5c8470ba3300d3fcd14a682
Sammy <sammyk118@gmail.com>
2021-09-02 13:36:15 -0700

    update

--- src/main/scala/cc/queries/infrequentJobs.scala
+++ src/main/scala/cc/queries/infrequentJobs.scala
@@ -1,5 +1,9 @@
 package cc.queries
 
+import cc.idx.{CCIdxMain, FilteredIndex, IndexUtil}
+
 object infrequentJobs extends Queries {
-    
+  def main(args: Array[String]) = {
+    CCIdxMain.main(args)
+  }
 }
