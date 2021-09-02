7e23f3b063c6d94dbc3fcdbada1dbc51bae8eedb
brian <brja3022@colorado.edu>
2021-09-02 10:18:49 -0600

    query for entries grouped by fetch date

--- src/main/scala/cc/queries/techJobs.scala
+++ src/main/scala/cc/queries/techJobs.scala
@@ -1,25 +1,24 @@
 package cc.queries
 
+import cc.idx.IndexUtil
 import cc.warc.WarcUtil
+import org.apache.spark.sql.functions.{col, to_date}
 import org.archive.archivespark.functions.{Html, HtmlText}
+import org.archive.archivespark.sparkling.cdx.CdxRecord
 import spark.session.AppSparkSession
 
 object techJobs extends Queries {
   def main(args: Array[String]): Unit = {
     val spark = AppSparkSession()
+    val df = spark.read.csv("s3a://maria-1086/Devin-Testing/outputs/test-write/")
 
-    val rdd=WarcUtil.load(path="s3a://commoncrawl/crawl-data/CC-MAIN-2014-23/segments/1405997885796.93/warc/CC-MAIN-20140722025805-00016-ip-10-33-131-23.ec2.internal.warc.gz")
-    println(rdd.take(1))
+    val withDate = df.withColumn("fetch_date", to_date(col("_c1"),"yyyy-MM-dd"))
 
-    val months = Array[String]("January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
-    var monthlyPages = Array[Int](0,0,0,0,0,0,0,0,0,0,0,0)
+    val grouped = withDate.groupBy("fetch_date").count().orderBy("fetch_date")
 
-    for (i <- 0 to 11) {
-
-      val xxt = rdd.enrich(HtmlText.ofEach(Html.all("body"))).toJsonStrings.take(1000).map { r => r.split(" ").mkString("Array(", ", ", ")").contains(months(i)) }
-
-      println(xxt.count(_ == true))
-    }
+    grouped.show()
+    //withDate.printSchema()
+    //withDate.show(1)
 
   }
     
