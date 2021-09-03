e90116af2d258ea3da3b665fb3562a6dc59bce75
bangumi4346 <70782220+bangumi4346@users.noreply.github.com>
2021-09-03 13:08:17 -0500

    with somewhat of an output

--- src/main/scala/cc/queries/largestJob.scala
+++ src/main/scala/cc/queries/largestJob.scala
@@ -2,7 +2,8 @@ package cc.queries
 
 import cc.idx.IndexUtil
 import cc.warc.{SuperWarc, WarcUtil}
-import org.apache.spark.sql.functions.{col, decode, desc, encode, exp, explode, lower}
+import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions.{col, desc, lower, length}
 import org.archive.archivespark.functions.{Html, HtmlText}
 import spark.session.AppSparkSession
 import sun.security.ssl.SSLContextImpl.DefaultSSLContext
@@ -21,23 +22,13 @@ object largestJob extends Queries {
 
     // load job CSV
     val csv = spark.read
-      //.format("csv")
+      .format("csv")
       .option("header", true)
-      .csv("s3a://maria-1086/FilteredIndex/CC-MAIN-2020-05/part-00000-451328f0-88e5-4cca-b2f8-703cd26ff421-c000.csv")
+      .load("s3a://maria-1086/FilteredIndex/*")
 
-    val files = csv.select(col("warc_filename"))
-
-    // query s3 cc path
-    // loop all and perform wordcount. append output to one df
-    var wordCount = spark.emptyDataFrame
-
- //   files.foreach{ i =>
-      //println("s3a://commoncrawl/" + i.toString.replace("[", "").replace("]",""))
-
-    //val rddAppend = WarcUtil.load("s3a://commoncrawl/" + i.toString.replace("[", "").replace("]",""))
-    val rddAppend = WarcUtil.load("s3a://commoncrawl/" + files.first().toString.replace("[", "").replace("]",""))
-      val wcAppend = spark.createDataFrame(
-        rddAppend
+    val rdd = WarcUtil.loadFiltered(csv)
+      val wordCount = spark.createDataFrame(
+        rdd
           .take(5)
           .map(warc => SuperWarc(warc))
           .flatMap(warc => warc.payload(true).split(" "))
@@ -46,8 +37,6 @@ object largestJob extends Queries {
           .map(pair => (pair._1, pair._2.map(_._2).reduce(_ + _)))
           .toSeq
       )
-      wordCount = wordCount.union(wcAppend)
-    //}
 
     //Step 2: filter for company names
     // 1. ascii only
@@ -55,22 +44,38 @@ object largestJob extends Queries {
     // 3. common english articles
     // 4. common words that is not a company name
     // the goal is top50 has only company names and top100 has mostly only company names
-/*
+
     wordCount
       .withColumnRenamed("_1", "word")
       .withColumnRenamed("_2", "count")
-      .withColumn("word", lower(col("word")))
+      //.withColumn("word", lower(col("word")))
 
       //ascii encode+decode filter
-      .withColumn("word", decode(encode(col("word") ,"ascii"),"ascii"))
-      .filter(!col("word").contains("?"))
+      //.withColumn("word", decode(encode(col("word") ,"ascii"),"ascii"))
+      //.filter(!col("word").contains("?"))
 
       //numbers and special characters
       .filter(!col("word").rlike("[0-9]"))
-      .filter(!col("word").rlike("-|[/,.|#()&!*:]"))
+      .filter(!col("word").rlike("-|[+></,.|’#'()&!*:@]"))
+
+      //filter lowercase
+      .filter(!(col("word")===lower(col("word"))))
+
+      //filter 2letters or below
+      .filter(!(length(col("word")) <= 2))
+
+      //common words
+      .filter(!col("word").rlike("(?i)and|the|in|of|to|a|for|as|you|with|for|is|our|your|be|an|us|will|or|have|on|are|free"))
+      .filter(!col("word").rlike("(?i)jobs|job|posted|experience|developer|by|get|it|we|help|but|from|home|site|resume|never|out"))
+      .filter(!col("word").rlike("(?i)test|log|hr|skills|scripts|per|while|such|which|ensure|degree|not|up|science|over|both|every"))
+      .filter(!col("word").rlike("(?i)I|reply|tips|first|some|my|if|new|how|comments|so|post|posts|follow|know|he|send|when|enter|recent"))
+      .filter(!col("word").rlike("(?i)most|mode|should|good|even|next|complete|project|school|no|let|old|guy|why|full|occurrence|nosql"))
 
       .orderBy(desc("count"))
-      .show(100)
+      //.show(100)
+      .repartition(1)
+      .write.format("csv").save("/output/output-"+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")))
+
 
     //Step 3: mapping common words to url
     // may have url linked to multiple words
@@ -79,7 +84,7 @@ object largestJob extends Queries {
     //Step 4: Query output
 
     //IndexUtil.write(wordCount_df, "s3://maria-1086/Hong-test/CCIdxMain/-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")), include_header=true, num_files=1)
-*/
+
     spark.stop
     System.exit(0)
   }
