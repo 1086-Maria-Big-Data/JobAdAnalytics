6d9cdc37740256e205e351653bb60ccd4380d4a0
Miguel <migcsomosot@gmail.com>
2021-09-03 13:15:41 -0500

    partitioned by month

--- src/main/scala/cc/queries/jobSpikes.scala
+++ src/main/scala/cc/queries/jobSpikes.scala
@@ -12,57 +12,32 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.{month, col}
 import org.apache.spark.sql.types.{StructType, StructField, IntegerType, LongType, StringType, TimestampType, ShortType}
 import cc.idx._
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.DataFrame
 import java.time.format.DateTimeFormatter
 import java.time.LocalDateTime
 
-
 object jobSpikes extends Queries {
-    val schema = StructType(Array(
-        StructField("url_surtkey", StringType, true),
-        StructField("fetch_time", TimestampType, true),
-        StructField("url", StringType, true),
-        StructField("content_mime_type", StringType, true),
-        StructField("fetch_status", ShortType, true),
-        StructField("content_digest", StringType, true),
-        StructField("fetch_redirect", StringType, true),        
-        StructField("warc_segment", StringType, true),
-        StructField("warc_record_length", IntegerType, true),
-        StructField("warc_record_offset", IntegerType, true),
-        StructField("warc_filename", StringType, true)
-    ))
-
+    
     def partitionByMonth(): Unit = {
         val spark = AppSparkSession()
-        val rddFile = spark.sparkContext.wholeTextFiles("s3a://maria-1086/Testing/Devin-Testing/outputs/test-write/*.csv", 24).flatMap(_._2.split("\n").drop(1).map(x=>x.split(" "))).map(x=>Row(x(0),LocalDateTime.parse(x(1).substring(0, 10), DateTimeFormatter.ISO_LOCAL_DATE),x(2),x(3),x(4),x(5),x(6),x(7),x(8).toInt,x(9).toInt,x(10)))
-        val DFFile = spark.createDataFrame(rddFile, schema)
-        DFFile.show()
-        // val testDF = spark.read.format("csv").schema(schema).option("header", "true").load("s3a://maria-1086/Testing/Mark-Testing/part-00001-bb6fa4ba-4e14-49d3-985c-e570505dc35d-c000.csv")
-        // // testDF.createOrReplaceTempView("testTable")
-        // val testDF1 = testDF.where(month(col("fetch_time"))===2)
-        // IndexUtil.write(testDF1, "s3a://maria-1086/TeamQueries/job-posting-spikes/2021//", include_header=true, num_files=1)
+        val df = spark.sqlContext.read.option("header", true).schema(IndexUtil.schema).csv("s3a://maria-1086/FilteredIndex/CC-MAIN-2021-**/*.csv")
+        var newDF = null.asInstanceOf[DataFrame]
+        for(x <- 1 to 12){
+            newDF = df.where(month(col("fetch_time"))===x)
+            IndexUtil.write(newDF, "s3a://maria-1086/TeamQueries/job-posting-spikes/2021/%02d".format(x), include_header=true, num_files=8)
+        }
     }
 
-    
+    def jobSpikesByJob(path:String): Unit = {
+        val spark = AppSparkSession()
+        val df = spark.read.format("csv").option("header", "true").load(path)
+        val df2 = 
+        df.createOrReplaceTempView("dat")
+        spark.sql("SELECT (select count(url) from dat where LOWER(url) like '%java%' and LOWER(url) not like '%javascript%') as java, (SELECT count(url) from dat where LOWER(url) like '%python%') as python, (SELECT count(url) from dat where LOWER(url) like '%scala%') as scala, (SELECT count(url) from dat where LOWER(url) like '%matlab%') as matlab, (SELECT count(url) from dat where LOWER(url) like '%sql%') as sql from dat limit 1").show()
+    }
+        
     def main(args: Array[String]): Unit = {
         val path = "s3a://maria-1086/Testing/Mark-Testing/part-00001-bb6fa4ba-4e14-49d3-985c-e570505dc35d-c000.csv"
         jobSpikesByJob(path)
-        
-        def jobSpikesByJob(path:String): Unit = {
-            val spark = AppSparkSession()
-            val df = spark.read.format("csv").option("header", "true").load(path)
-            val df2 = 
-            df.createOrReplaceTempView("dat")
-            spark.sql("SELECT (select count(url) from dat where LOWER(url) like '%java%' and LOWER(url) not like '%javascript%') as java, (SELECT count(url) from dat where LOWER(url) like '%python%') as python, (SELECT count(url) from dat where LOWER(url) like '%scala%') as scala, (SELECT count(url) from dat where LOWER(url) like '%matlab%') as matlab, (SELECT count(url) from dat where LOWER(url) like '%sql%') as sql from dat limit 1").show()
-        
-        // val df_covid_19_data = spark.read.format("csv").option("header", "true").load("input/covid_19_data.csv")
-        //val rddFromFile = spark.sparkContext.textFile("s3a://maria-1086/Testing/Mark-Testing/part-00001-bb6fa4ba-4e14-49d3-985c-e570505dc35d-c000.csv",2)
-        //val rdd = rddFromFile.map(f =>{f.split(",")})
-        //rddFromFile.foreach(println)
-        //rddFromFile.toDF()
-        //val df2 = df.filter(col("url").like("%software%"))
-        
-        
-        }
-     }
+    }
 }
