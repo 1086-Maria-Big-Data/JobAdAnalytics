e3dbbbc3cafc1b9a46b03794af83a73e66cfd422
williamgantterry <williamgantterry@gmail.com>
2021-09-03 16:13:32 -0400

    will hopefully last version on his end for EMR entryLevel

--- src/main/scala/cc/queries/entryLevel.scala
+++ src/main/scala/cc/queries/entryLevel.scala
@@ -2,15 +2,8 @@ package cc.queries
 
 import cc.warc._
 import spark.session.AppSparkSession
-import org.archive.archivespark._
-import org.archive.archivespark.functions._
-import org.archive.archivespark.specific.warc._
-import org.archive.archivespark.specific.warc.functions._
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types._
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.conf.Configuration
 import java.net.URI
 import org.apache.spark.deploy.SparkHadoopUtil
 
@@ -20,7 +13,7 @@ object entryLevel extends Queries {
 
 
   def main(args: Array[String]): Unit = {
-
+   //xxt=rdd2.take(750) can be changed to desired number and output file should have correct location at bottom
     val spark = AppSparkSession()
 
     var list1:List[Double]=List()
@@ -36,15 +29,15 @@ object entryLevel extends Queries {
     val pathList:Array[String]=status.map(x=> x.getPath.toString).filter(x=>x.contains(".csv"))
     //pathList.foreach(println)
 
-    //for(i<- 0 to (pathList.length-1)){
-    for(i<- 0 to 2){
+    for(i<- 0 to (pathList.length-1)){
+    //for(i<- 0 to 5){
       println(pathList(i))
       val pathToUse:String=pathList(i)
       val rdd2= WarcUtil.loadFiltered(spark.read.option("header", true).csv(path=pathToUse),enrich_payload=true)
-      val xxt=rdd2.take(1000).map(x1=>SuperWarc(x1)).map{r =>r.payload(textOnly = true)}.filter{
-        f=> f.contains("entry-level")|| f.contains("entry level") }
-      val xxt2=xxt.filter(f=> f.contains("experience"))
-      val xxt3=xxt2.filter(f=> !f.contains("no experience") )
+      val xxt=rdd2.take(750).map(x1=>SuperWarc(x1)).map{r =>r.payload(textOnly = true)}.filter{
+        f=> f.matches(".*[Ee]ntry-[Ll]evel.*")|| f.matches(".*[Ee]ntry [Ll]evel.*") }
+      val xxt2=xxt.filter(f=> f.matches(".*[Ee]xperience.*"))
+      val xxt3=xxt2.filter(f=> !f.matches(".*[Nn]o [Ee]xperience.*"))
 
       list1=list1++List(xxt.length.toDouble)
       list2=list2++List(xxt3.length.toDouble)
@@ -56,8 +49,8 @@ object entryLevel extends Queries {
     val Total=(list1,list2,list3).zipped.toList
     val dftoWrite = spark.createDataFrame(Total).toDF("Total Entry-Level Tech Jobs", "Entry-Level Exp. Req.","Percent Requiring Exp.")
     dftoWrite.show()
-    dftoWrite.coalesce(1).write.format("csv").option("header","true").mode("Overwrite").save("/output/testing")
-    //dftoWrite.coalesce(1).write.format("csv").option("header", "true").mode("Overwrite").save("s3a://maria-1086/Testing/will_testing/newresults")
+    //dftoWrite.coalesce(1).write.format("csv").option("header","true").mode("Overwrite").save("/output/testing")
+    dftoWrite.coalesce(1).write.format("csv").option("header", "true").mode("Overwrite").save("s3a://maria-1086/Testing/will_testing/newresults")
     //dftoWrite.coalesce(1).write.format("csv").option("header", "true").mode("Overwrite").save("s3a://maria-1086/TeamQueries/entryLevel")
 
     spark.stop
