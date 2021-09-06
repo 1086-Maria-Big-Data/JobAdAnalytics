5689e30d32bb54108982aeca6631cafc8a2d0263
Miguel <migcsomosot@gmail.com>
2021-09-06 13:27:09 -0500

    added count by quarter

--- src/main/scala/cc/queries/jobSpikes.scala
+++ src/main/scala/cc/queries/jobSpikes.scala
@@ -9,7 +9,7 @@ import org.archive.archivespark.functions._
 import org.archive.archivespark.specific.warc._
 import org.archive.archivespark.specific.warc.functions._
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.functions.{year, month, col}
+import org.apache.spark.sql.functions.{quarter, year, month, col}
 import org.apache.spark.sql.types.{StructType, StructField, IntegerType, LongType, StringType, TimestampType, ShortType}
 import cc.idx._
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -32,8 +32,32 @@ object jobSpikes extends Queries {
 
         IndexUtil.writeParquet(df, "s3a://maria-1086/TeamQueries/job-posting-spikes/parquet", partition_cols=Seq("fetch_year", "fetch_month"))
     }
-    def countByMonth(spark: SparkSession): Unit ={
-        val df = spark.sqlContext.read.option("header", true).schema(IndexUtil.schema).parquet("s3://maria-1086/TeamQueries/job-posting-spikes/2021/**/*.parquet")
+    // def partitionByQuarter(spark: SparkSession): Unit ={
+    //     val df = spark.sqlContext
+    //         .read
+    //         .option("header", true)
+    //         .schema(IndexUtil.schema)
+    //         .csv("s3a://maria-1086/FilteredIndex/CC-MAIN-202*-**/*.csv")
+    //         .withColumn("fetch_quarter", quarter(col("fetch_time")))
+
+        
+    // }
+    def countByMonth(spark: SparkSession): DataFrame ={
+        val df = spark.sqlContext.read.option("header", true)
+            .schema(IndexUtil.schema)
+            .parquet("s3a://maria-1086/TeamQueries/job-posting-spikes/parquet/")
+        var newDF = df.groupBy("fetch_year", "fetch_month").count().orderBy("fetch_year", "fetch_month")
+        return newDF
+    }
+    def countByQuarter(spark: SparkSession): DataFrame ={
+        val df = spark.sqlContext
+            .read.option("header", true)
+            .schema(IndexUtil.schema)
+            .parquet("s3a://maria-1086/TeamQueries/job-posting-spikes/parquet/")
+            .withColumn("fetch_quarter", quarter(col("fetch_time")))
+        var quarterDF = df.groupBy("fetch_year", "fetch_quarter").count().orderBy("fetch_year", "fetch_quarter")
+        return quarterDF
+        
     }
     def jobSpikesByJob(spark: SparkSession): Unit = {
         //val df = spark.sqlContext.read.option("header", true).schema(IndexUtil.schema).csv("s3a://maria-1086/TeamQueries/job-posting-spikes/2021/**/*.csv")
@@ -48,11 +72,9 @@ object jobSpikes extends Queries {
     def main(args: Array[String]): Unit = {
         // jobSpikesByJob()
         val spark = AppSparkSession()
-        val df = spark.sqlContext
-        .read
-        .option("basePath", "s3a://maria-1086/TeamQueries/job-posting-spikes/parquet")
-        .parquet("s3a://maria-1086/TeamQueries/job-posting-spikes/parquet/")
-
-        df.show
+        var mainDF = countByMonth(spark)
+        // val mainDF = countByQuarter(spark)
+        IndexUtil.write(mainDF, "s3a://maria-1086/TeamQueries/job-posting-spikes/output-miguel/countByMonths", num_files = 1, include_header = true)
     }
 }
+
