6f8b26303262057f01ab1396fbf9d768c96b8af8
glassbats <glassbats@gmail.com>
2021-09-06 16:49:59 -0700

    Added our fallback query to largestsJob

--- src/main/scala/cc/queries/largestJob.scala
+++ src/main/scala/cc/queries/largestJob.scala
@@ -1,14 +1,29 @@
 package cc.queries
 
-import cc.idx.FilteredIndex.techJobTerms
-import cc.idx.{CCIdxMain, FilteredIndex, IndexUtil}
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, udf}
+import cc.idx.IndexUtil
+import org.apache.spark.sql.functions._
 import spark.session.AppSparkSession
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object largestJob extends Queries {
   def main(args: Array[String]) = {
-    IndexUtil.write(FilteredIndex.filter("CC-MAIN-2021-10"), "s3a://maria-1086/FilteredIndex/CC-MAIN-2021-10", " ", true, 24)
+    val spark = AppSparkSession()
+
+    import spark.implicits._
+
+    val df = spark.sqlContext.read
+      .option("header", true)
+      .schema(IndexUtil.schema)
+      .csv("s3a://maria-1086/FilteredIndex/CC-MAIN-2021-**/*.csv")
+
+    df.groupBy("url_host_name")
+      .count
+      .orderBy(desc("count"))
+      .show(20, false)
+    
+    //IndexUtil.write(df, "s3://maria-1086/Testing/JesseTesting/outputs/" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")), include_header=true, num_files = 1)
   }
   override def run(): Unit = {
 
